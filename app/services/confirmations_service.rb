43b735b1d6491380580ef10182392a4ec3dc91c2
nhannt-itech <nhannt.itech@gmail.com>
2022-03-09 10:33:21 +0700

    update: fix search command

--- app/services/confirmations_service.rb
+++ app/services/confirmations_service.rb
@@ -1,6 +1,9 @@
 class ConfirmationsService
 	def self.confirm_email?(token)
-		verification = Verification.find_by(status: 'pending', token: token)
+		verification =
+			Verification
+				.where(status: :pending, verificationable_type: :ConfirmEmail)
+				.find_by(status: 'pending')
 		if verification && ((verification.created_at + Verification::TOKEN_LIFETIME) > Time.now)
 			verification.user.update(email_confirmed_at: Time.now)
 			verification.update(status: :done)
