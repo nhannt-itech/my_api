dff69f7d55102b169c749a283bb3dd48584ae27f
nhannt-itech <nhannt.itech@gmail.com>
2022-03-10 16:09:02 +0700

    annotate model

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  full_address     :string
+#  addressable_type :string           not null
+#  addressable_id   :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class AddressTest < ActiveSupport::TestCase
--- test/models/auction_item_test.rb
+++ test/models/auction_item_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: auction_items
+#
+#  id            :bigint           not null, primary key
+#  title         :string
+#  price         :float
+#  shipping_cost :float
+#  bids          :integer
+#  end_at        :datetime
+#  user_id       :bigint           not null
+#  avatar        :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 require "test_helper"
 
 class AuctionItemTest < ActiveSupport::TestCase
